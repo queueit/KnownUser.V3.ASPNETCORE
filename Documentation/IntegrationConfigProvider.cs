817faf0ee60c4fdc056c545abd325ee64647a211
Frederik Williams <frwi@queue-it.com>
2021-01-08 15:33:36 +0100

    Updated IntegrationConfigProvider to use System.Text.Json and modern c# features

--- Documentation/IntegrationConfigProvider.cs
+++ Documentation/IntegrationConfigProvider.cs
@@ -1,8 +1,8 @@
-using Newtonsoft.Json;
 using QueueIT.KnownUser.V3.AspNetCore.IntegrationConfig;
 using System;
 using System.IO;
 using System.Net;
+using System.Text.Json;
 using System.Timers;
 
 namespace QueueIT.KnownUserV3.SDK.IntegrationConfigLoader
@@ -14,7 +14,6 @@ namespace QueueIT.KnownUserV3.SDK.IntegrationConfigLoader
     /// </summary>
     public static class IntegrationConfigProvider
     {
-
         private const int _downloadTimeoutMS = 4000;
         internal static int _RefreshIntervalS = 5 * 60;
         internal static double _RetryExceptionSleepS = 5;
@@ -24,59 +23,67 @@ namespace QueueIT.KnownUserV3.SDK.IntegrationConfigLoader
         private static bool _isInitialized = false;
         private static string _customerId;
         private static string _apiKey;
+
         public static CustomerIntegration GetCachedIntegrationConfig(string customerId, string apiKey)
         {
-            if(!_isInitialized)
+            if (!_isInitialized)
             {
                 _customerId = customerId;
                 _apiKey = apiKey;
+
                 lock (_lockObject)
                 {
                     if (!_isInitialized)
                     {
-                        RefreshCache(init:true);
-                        _timer = new Timer();
-                        _timer.Interval = _RefreshIntervalS * 1000;
-                        _timer.AutoReset = false;
+                        RefreshCache(init: true);
+                        _timer = new Timer
+                        {
+                            Interval = _RefreshIntervalS * 1000,
+                            AutoReset = false
+                        };
                         _timer.Elapsed += TimerElapsed;
                         _timer.Start();
                         _isInitialized = true;
                     }
                 }
             }
+
             return _cachedIntegrationConfig;
         }
 
-   
         private static void TimerElapsed(object sender, ElapsedEventArgs e)
         {
-            RefreshCache(init:false);
+            RefreshCache(init: false);
             _timer.Start();
         }
 
         private static void RefreshCache(bool init)
         {
             int tryCount = 0;
+            var configUrl = $"https://{_customerId}.queue-it.net/status/integrationconfig/secure/{_customerId}";
+
             while (tryCount < 5)
             {
-                var configUrl = string.Format("https://{0}.queue-it.net/status/integrationconfig/secure/{0}", _customerId);
                 try
                 {
                     HttpWebRequest request = (HttpWebRequest)WebRequest.Create(configUrl);
                     request.Headers.Add("api-key", _apiKey);
                     request.Timeout = _downloadTimeoutMS;
-                    using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+
+                    using HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+                    if (response.StatusCode != HttpStatusCode.OK)
                     {
-                        if (response.StatusCode != HttpStatusCode.OK)
-                            throw new Exception($"It was not sucessful retriving config file status code {response.StatusCode} from {configUrl}");
-                        
-                        using (StreamReader reader = new StreamReader(response.GetResponseStream()))
-                        {
-                             var deserialized = JsonConvert.DeserializeObject<CustomerIntegration>(reader.ReadToEnd());
-                            _cachedIntegrationConfig = deserialized ?? throw new Exception("CustomerIntegration is null");
-                        }
-                        return;
+                        throw new Exception(
+                            $"It was not sucessful retriving config file status code {response.StatusCode} from {configUrl}");
                     }
+
+                    using StreamReader reader = new StreamReader(response.GetResponseStream());
+                    var deserialized = JsonSerializer.Deserialize<CustomerIntegration>(reader.ReadToEnd());
+
+                    _cachedIntegrationConfig = deserialized ?? throw new Exception("CustomerIntegration is null");
+
+                    return;
                 }
                 catch (Exception ex)
                 {
